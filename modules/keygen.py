97f54b4db70cce5f0be2bf67d867394d49f7363c
Mikhail Komarov <144356904+hardWorker254@users.noreply.github.com>
2025-02-27 20:01:26 +0400

    Update keygen.py
    Deleted unusual imports and other

--- modules/keygen.py
+++ modules/keygen.py
@@ -1,30 +1,7 @@
-import json
 import random
-import os
 
 
-from modules.constants import KEYS_FILE, USERS_FILE
-from modules.loc_io import load_data, save_data
-
-
-def load_keys():
-    return load_data(KEYS_FILE)
-
-
-def save_keys(keys):
-    save_data(KEYS_FILE, keys)
-
-
-def load_users():
-    if (not os.path.exists(USERS_FILE) or not os.path.getsize(USERS_FILE)) > 0:
-        return {}
-    with open(USERS_FILE, "r") as file:
-        return json.load(file)
-
-
-def save_users(users):
-    with open(USERS_FILE, "w") as file:
-        json.dump(users, file)
+from modules.loc_io import load_keys, save_keys
 
 
 def generate_key(name):
