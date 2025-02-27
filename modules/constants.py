b76fefedda9999340b65d4e9c645a81a4fac07b8
Mikhail Komarov <144356904+hardWorker254@users.noreply.github.com>
2025-02-27 19:11:50 +0400

    Create constants.py

---
+++ modules/constants.py
@@ -0,0 +1,10 @@
+# File Paths
+USERS_FILE = "users.json"
+CHATS_FILE = "chats.json"
+MESSAGES_FILE = "db.json"
+CHANNELS_FILE = "channels.json"
+KEYS_FILE = "keys.json"
+MEDIA_FOLDER = 'media'
+
+# Other
+MESSAGE_LIMIT = 10
