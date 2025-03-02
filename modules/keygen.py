06d77c501a092ef6045a70fffc99d1d5cb312273
Mikhail Komarov <144356904+hardWorker254@users.noreply.github.com>
2025-03-02 20:17:33 +0400

    Update keygen.py

--- modules/keygen.py
+++ modules/keygen.py
@@ -1,16 +1,9 @@
 import random
 
 
-from modules.loc_io import load_keys, save_keys
+def generate_key():
+    key = random.randint(0, 
+        9999999999999999)
+    key = str(key).zfill(16)
+    return key
 
-
-def generate_key(name):
-    keys = load_keys()
-    if name in keys:
-        print(keys[name])
-    else:
-        key = random.randint(0, 
-            9999999999999999)
-        key = str(key).zfill(16)
-        keys[name] = key
-        save_keys(keys)
